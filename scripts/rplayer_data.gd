39232d820401e121d64ba6833de26887abfcb4e9
arsen-lovecraft <50301289+Arsen-Lovecraft@users.noreply.github.com>
2025-02-20 12:04:02 +0300

    animation rework. SpriteAnimation2D is removedd

--- scripts/rplayer_data.gd
+++ scripts/rplayer_data.gd
@@ -3,13 +3,23 @@ extends Resource
 
 signal player_dead()
 
-@export var speed: float = 150
+@export var velocity: float = 150:
+	set(value):
+		if(value <= 0):
+			velocity = 0
+		else:
+			velocity = value
 @export var hp: int = 2:
 	set(value):
 		if(value <= 0):
 			player_dead.emit()
 		hp = value
-@export var steering_velocity: float = 160
+@export var steering_velocity: float = 160:
+	set(value):
+		if(value <= 0):
+			steering_velocity = 0
+		else:
+			steering_velocity = value
 
 @export var smash_cooldown: float = 3.0
 @export var smash_power: int = 2
